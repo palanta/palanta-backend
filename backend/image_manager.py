ace2ec75e171f76281473fca97fa50ae12dd31d9
Julien Scholz <julienscholzo@hotmail.de>
2020-01-15 09:03:38 +0100

    Fix image upload not working properly

--- backend/image_manager.py
+++ backend/image_manager.py
@@ -1,9 +1,11 @@
 import cv2
 import hashlib
 import os
+import uuid
 
 IMAGE_DIRECTORY = os.path.abspath('./images')
-
+if not os.path.exists(IMAGE_DIRECTORY):
+    os.makedirs(IMAGE_DIRECTORY)
 
 def _hash(image):
     h = hashlib.sha1()
@@ -26,14 +28,17 @@ def load(image_id):
 def save(image):
     image_id = _hash(image)
     filename = get_path_from_id(image_id)
-    if not os.path.exists(IMAGE_DIRECTORY):
-        os.makedirs(IMAGE_DIRECTORY)
     cv2.imwrite(filename, image)
     return image_id
 
 
-def upload(filename):
-    image = cv2.imread(filename)
+def upload(file):
+    temp_filename = get_path_from_id(str(uuid.uuid4()))
+    file.save(temp_filename)
+    print('saved as ', temp_filename)
+    image = cv2.imread(temp_filename)
+    print('loaded as ', temp_filename)
+    os.remove(temp_filename)
     if image is None:
         return None
     return save(image)
--- backend/images.py
+++ backend/images.py
@@ -3,13 +3,25 @@ from flask import request, send_file
 from . import app
 from . import image_manager
 
+UPLOAD_FOLDER = './tmp/'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png', 'gif', 'bmp'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 @app.route('/images', methods=['POST'])
 def upload():
     if len(request.files) != 1:
         return 'invalid_file_count', 400
-    file = list(request.files.values())[0].filename
-    print(file)
+    file = list(request.files.values())[0]
+    if file.filename == '':
+        return 'no_selected_file', 400
+    if not allowed_file(file.filename):
+        return 'unsupported_extension', 400
     image_id = image_manager.upload(file)
     if image_id is None:
         return 'internal_server_error', 500
